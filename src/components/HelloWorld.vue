f33c2f7eaef14914bad3f6bf3cedda0a3a6d248b
Thanawat Thangpiboonvech <thanawat.t@dv.co.th>
2021-09-26 17:18:13 +0700

    add resume

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,10 @@
     <h3>I’m <span class="heading">Spite</span></h3>
     <h1 class="white-text">{{ job }}</h1>
     <p class="second-text">I’m a Full-Stack Front-End Developer based in Bangkok, Thailand.</p>
+    <br />
+    <div class="download-btn">
+      <div class="btn"><a href="/resume-thanawat.pdf" download>Download CV</a></div>
+    </div>
   </div>
 </template>
 
@@ -32,6 +36,59 @@ export default defineComponent({
   .heading {
     margin-left: 1rem;
     font-size: 5rem;
+  }
+
+  .download-btn {
+    cursor: pointer;
+    width: 30%;
+    background: linear-gradient(to right, #ffce00 0%, #00ffce 75%, #ce00ff 100%);
+    background-size: 500%;
+    border: none;
+    border-radius: 2rem;
+    padding: 0.2rem;
+    text-align: center;
+
+    animation-name: gradientOut;
+    animation-duration: 0.5s;
+
+    &:hover {
+      animation-name: gradient;
+      animation-iteration-count: 1;
+      animation-fill-mode: forwards;
+    }
+    &:focus {
+      outline: none;
+    }
+
+    .btn {
+      border-radius: 2rem;
+      width: 100%;
+      height: 100%;
+      background-color: #000;
+      padding: 0.5rem 0;
+
+      a {
+        color: #ffce00;
+        text-decoration: none;
+      }
+    }
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 100%;
+    }
+  }
+  @keyframes gradientOut {
+    0% {
+      background-position: 100%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
   }
 }
 </style>
